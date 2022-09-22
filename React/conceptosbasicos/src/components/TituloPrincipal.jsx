83e0a900623adcdf492a96676334aceedc0b3b72
Lucia Soraire <luciasoraire@gmail.com>
2022-09-22 00:28:01 -0300

    Agregué el punto 3

--- React/conceptosbasicos/src/components/TituloPrincipal.jsx
+++ React/conceptosbasicos/src/components/TituloPrincipal.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 const TituloPrincipal = (props) => {
+const [change, setChange] = useState("");
+const show = ()=>{
+    setChange("(from changed state)")
+}
+
     return (
-        <h1>¡Hello {props.codigoFriend}!</h1>
+        
+        <>
+        <h1>¡Hello {props.codigoFriend} {change}!</h1>
+        <button onClick={show}>Click me</button>
+        </>
     );
 };
 
